9db2969d6052d6e0ee530fae993f87fa45700d3e
Jon Kuperman <jon.kuperman@gmail.com>
2014-06-19 19:22:38 -0700

    sometimes fixed position base functionality

---
+++ README.md
@@ -0,0 +1,6 @@
+# Sometimes Fixed Position
+
+This is a simple jQuery plugin that allows you to turn any element into fixed position only when the screen scrolls to it.
+
+## Usage
+$('.element').sometimes();
---
+++ sometimes.js
@@ -0,0 +1,15 @@
+(function ($) {
+    $.fn.sometimes = function() {
+        $element = $(this);
+
+        $(window).scroll(function() {
+        	if ($(window).scrollTop() >= $element.offset().top) {
+        		$element.css({"position":"fixed","top":0});	
+        	} else {
+        		$element.removeAttr('style');
+        	}
+        });
+    };
+}(jQuery));   
+
+$('.sometimes').sometimes();
