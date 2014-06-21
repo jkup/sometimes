0c88ffa3d97983b5f4f8c0ddc7fd22d9268de5aa
Jon Kuperman <jon.kuperman@gmail.com>
2014-06-21 15:24:20 -0700

    John Resig style scroll event

--- sometimes.js
+++ sometimes.js
@@ -1,18 +1,28 @@
 (function ( $ ) {
     $.fn.sometimes = function( styles ) {
-        $element = $(this);
+        var didScroll = false;
+        var $element = $(this);
 
         $(window).scroll(function() {
-            // If the window has scrolled to the top of the element
-            if ($(window).scrollTop() >= $element.offset().top) {
-                $element.css({"position":"fixed","top":0});
-
-                // If additional styles were passed in, apply them now
-                if (typeof(styles) === 'object') { $element.css(styles); }
-            } else {
-                $element.removeAttr('style');
-            }
+            didScroll = true;
         });
+
+        setInterval(function() {
+            if ( didScroll ) {
+                
+                didScroll = false;
+
+                // If the window has scrolled to the top of the element
+                if ($(window).scrollTop() >= $element.offset().top) {
+                    $element.css({"position":"fixed","top":0});
+
+                    // If additional styles were passed in, apply them now
+                    if (typeof(styles) === 'object') { $element.css(styles); }
+                } else {
+                    $element.removeAttr('style');
+                }
+            }
+        }, 250);
 
         return this;
     };
